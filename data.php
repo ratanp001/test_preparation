9c17cbaffa98e884f43f5c28bbfa12d70f548298
Ratan Parmar <ratanp001@gmail.com>
2018-01-22 16:14:32 +0530

    initial project version

---
+++ data.php
@@ -0,0 +1,9 @@
+<?php
+  
+    if(isset($_POST['submit'])){
+    $names= $_POST['name'];
+
+	echo "hi".$names;
+	}
+
+?>
---
+++ form.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<HTML>
+
+<body>
+	<form action="data.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="password"><br>
+<input type="submit" name="submit">
+</form>
+</body>
+</HTML>
